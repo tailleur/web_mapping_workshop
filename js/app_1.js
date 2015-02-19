aa31ae4e9a55518e5481fb78fcda8e5ff4c61137
tailleur <mtaylor@springfieldmo.gov>
2015-02-19 15:14:49 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -16,7 +16,7 @@ var dataFileToAdd = 'data/bikeway.geojson';
 
 var featureLayer = L.mapbox.featureLayer();
 
-  featureLayer.loadUrl(dataFileToAdd);
+  featureLayer.loadURL(dataFileToAdd);
   featureLayer.addTo(map);
   
 featureLayer.on('ready', function(){
